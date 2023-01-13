d046d1c3f6bf0ca50b37ddf49d9a842590eaa9d7
paisssszz <faizf5729@gmail.com>
2023-01-13 08:56:36 +0700

    pais

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,13 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="row">
         <div class="col-md-9">
             <a href="/tambah_detail">upload</a>
             <table style="overflow:hidden;">
-                <thead>
+                {{-- <thead>
                     <tr>
                         <th>ID</th>
                         <th>Preview Item</th>
@@ -22,15 +23,16 @@
                         <th>Title</th>
                         <th>Description</th>
                     </tr>
-                </thead>
+                </thead> --}}
 
                 <tbody>
                     @php
                         $no = 1;
                     @endphp
+                    <div class="container my-5 d-flex justify-content-evenly flex-wrap">
                     @foreach ($data as $row)
 
-                    <tr>
+                    {{-- <tr>
                         
                         <th scope="row">{{$no++}}</th>
                         <th>{{$row->preview_item}}</th>
@@ -40,8 +42,24 @@
                         <th>{{$row->expiration_date}}</th>
                         <th>{{$row->title}}</th>
                         <th>{{$row->deskrpsi}}</th>
-                    </tr>
+                    </tr> --}}
+                    <div class="card my-5" style="width: 25rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">{{$row->preview_item}}</h5>
+                        <div class="my-3 d-flex justify-content-lg-around align-items-center">
+                            <p class="card-text">{{$row->expiration_date}}</p>
+                            <p class="card-text">{{$row->price}}</p>
+                            
+                        </div>
+                        <div class="my-5 d-flex align-items-center justify-content-between">
+                            <a href="#" class="btn btn-primary">Place a Bid</a>
+                            <a href="" class="btn btn-light">View Detail</a>
+                        </div>
+                        </div>
+                      </div>
                     @endforeach
+                </div>
                 </tbody>
             </table>
         </div>
